9615e44f4f88de5399eeeb3a20d757006fcf4ca8
facuperezm <facundoperezmontalvo@gmail.com>
2024-03-15 17:03:08 -0300

    Add langchain dependency and refactor main function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,50 @@
 import contentTest from "@/utils/contents";
 import { openai } from "@/utils/openai";
-import { createClient } from "@/utils/supabase/server";
 import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
 export default async function Home() {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-
-  async function main(input: string[]) {
-    const data = await Promise.all(
-      input.map(async (textChunk) => {
-        const embeddingResponse = await openai.embeddings.create({
-          model: "text-embedding-ada-002",
-          input: textChunk,
-        });
-        return {
-          content: textChunk,
-          embedding: embeddingResponse.data[0].embedding,
-        };
-      })
-    );
-
-    await supabase.from("documents").insert(data);
-  }
-
   // Create a new row in the "posts" table
   async function create(fd: FormData) {
     "use server";
+
     const supabase = createServerActionClient({ cookies });
 
-    const text = fd.get("text");
-    const { data, error } = await supabase.from("todos").insert({ text });
+    async function main(input: any) {
+      const data = await Promise.all(
+        input.map(async (textChunk: any) => {
+          const embeddingResponse = await openai.embeddings.create({
+            model: "text-embedding-ada-002",
+            input: textChunk,
+          });
+          return {
+            content: textChunk,
+            embedding: embeddingResponse.data[0].embedding,
+          };
+        })
+      );
+
+      // Insert content and embedding into Supabase
+      await supabase.from("documents").insert(data);
+      console.log("Embedding and storing complete!");
+    }
+
+    main(contentTest);
+    // const supabase = createServerActionClient({ cookies });
+
+    // const text = fd.get("text");
+    // const { data, error } = await supabase.from("todos").insert({ text });
   }
 
   return (
-    <form action={create}>
-      <label htmlFor="text">
-        TEXT
-        <input type="text" name="text" />
-      </label>
-      <button type="submit">add</button>
-    </form>
+    <>
+      <form action={create}>
+        <label htmlFor="text">
+          TEXT
+          <input type="text" name="text" />
+        </label>
+        <button type="submit">add</button>
+      </form>
+    </>
   );
 }
